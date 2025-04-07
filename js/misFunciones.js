791428b5804f9ca801141fa9c7806a6065f68575
Genealizacion <144199757+INEGI-Python@users.noreply.github.com>
2025-04-07 20:34:05 +0000

    comilllas normales

--- js/misFunciones.js
+++ js/misFunciones.js
@@ -4,10 +4,10 @@ class generalizacion{
     }
     Ayuda(){
             return {"reducePuntos":idioma=>{
-                                    console.log(´Ayuda separa  Lineas en ${idioma}´)
+                                    console.log("Ayuda separa  Lineas en "+ idioma)
                     },
                     "separaLineas": idioma => {
-                        console.log(´Ayuda separa  Lineas en ${idioma}´)
+                        console.log("Ayuda separa  Lineas en "+ idioma)
                     }
             }[this.metodo]
         }
