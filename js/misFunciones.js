c1fd868f5d0058bc9faf2c041358d41f1fb5330c
Genealizacion <144199757+INEGI-Python@users.noreply.github.com>
2025-04-07 20:26:10 +0000

    clase generalizacion pruebas

---
+++ js/misFunciones.js
@@ -0,0 +1,14 @@
+class generalizacion{
+    constructor(metodo){
+        this.metodo = metodo        
+    }
+    Ayuda(){
+            return {"reducePuntos":idioma=>{
+                                    console.log(´Ayuda separa  Lineas en ${idioma}´)
+                    },
+                    "separaLineas": idioma => {
+                        console.log(´Ayuda separa  Lineas en ${idioma}´)
+                    }
+            }[this.metodo]
+        }
+}
